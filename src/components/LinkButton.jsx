a9a2f5f637e2b6dc04788d7046f5278bdafc51c5
goodness <goodyc474@gmail.com>
2022-10-29 15:13:22 +0100

    Add Tooltip to LinkButton component

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,7 +1,14 @@
-const LinkButton = ({ children, id,  href, tooltipTitle, tooltipSubtext }) => {
-    return <button id={id}>
-    <a href={href}>{children}</a>
-  </button>
+import Tooltip from "./Tooltip.jsx";
+
+const LinkButton = ({ children, id, href, tooltipTitle, tooltipSubtext }) => {
+  return (<>
+    <button id={id}>
+      <a href={href}>{children}</a>
+    </button>
+
+    <Tooltip title={tooltipTitle} subText={tooltipSubtext} />
+  </>
+  )
 }
 
 export default LinkButton;
